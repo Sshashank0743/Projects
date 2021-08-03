9e5796932d40f564136587f2695046f9b5598723
shashank shukla <53620609+Sshashank0743@users.noreply.github.com>
2021-08-03 12:20:49 +0530

    Add files via upload

---
+++ Music_Player/mp3Player.py
@@ -0,0 +1,30 @@
+# Music player in python
+
+from pygame import mixer
+
+mixer.init()
+# --------------------------Path of your music
+mixer.music.load("C:/Users/sshas/OneDrive/Desktop/NAGIN Theme DJ mix.mp3")
+mixer.music.set_volume(0.5)
+mixer.music.play()
+
+while True:
+    print("Press 'p' to pause")
+    print("Press 'r' to resume")
+    print("Press 'v' set volume")
+    print("Press 'e' to exit")
+
+    ch = input("['p','r','v','e']>>>")
+
+    if ch == "p":
+        mixer.music.pause()
+    elif ch == "r":
+        mixer.music.unpause()
+    elif ch == "v":
+        v = float(input("Enter volume(0 to 1): "))
+        mixer.music.set_volume(v)
+    elif ch == "e":
+        mixer.music.stop()
+        break
+
+# Follow @code_snail
